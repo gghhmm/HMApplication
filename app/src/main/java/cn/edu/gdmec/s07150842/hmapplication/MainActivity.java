fa45eb0fb0a38a4d21d28f6ed5f28f00fee3b3a2
heyman yang <heyman yang>
2016-09-12 11:20:33 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150842/hmapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.s07150842.hmapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
